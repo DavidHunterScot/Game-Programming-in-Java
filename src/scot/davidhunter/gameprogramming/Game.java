fa2f891cb822ebf23f1a5aa2639a1c418c0d74ea
DavidHunterScot <contact@davidhunter.scot>
2022-11-06 18:26:04 +0000

    Episode 17 - Key Input

--- src/scot/davidhunter/gameprogramming/Game.java
+++ src/scot/davidhunter/gameprogramming/Game.java
@@ -10,6 +10,7 @@ import java.awt.image.DataBufferInt;
 import javax.swing.JFrame;
 
 import scot.davidhunter.gameprogramming.graphics.Screen;
+import scot.davidhunter.gameprogramming.input.Keyboard;
 
 public class Game extends Canvas implements Runnable
 {
@@ -22,6 +23,7 @@ public class Game extends Canvas implements Runnable
 	
 	private Thread thread;
 	private JFrame frame;
+	private Keyboard key;
 	private boolean running = false;
 	
 	private Screen screen;
@@ -35,8 +37,10 @@ public class Game extends Canvas implements Runnable
 		setPreferredSize( size );
 		
 		screen = new Screen( width, height );
-		
 		frame = new JFrame();
+		key = new Keyboard();
+		
+		addKeyListener( key );
 	}
 	
 	public synchronized void start()
@@ -103,8 +107,10 @@ public class Game extends Canvas implements Runnable
 	
 	public void update()
 	{
+		key.update();
+		
 		x++;
-		// y++;
+		y++;
 	}
 	
 	public void render()
