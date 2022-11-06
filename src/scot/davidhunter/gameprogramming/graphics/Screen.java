be88fc0ead1505926506efe1bb37baf4968d69de
DavidHunterScot <contact@davidhunter.scot>
2022-11-06 20:53:40 +0000

    Episode 29 - The Render Tile Method

--- src/scot/davidhunter/gameprogramming/graphics/Screen.java
+++ src/scot/davidhunter/gameprogramming/graphics/Screen.java
@@ -62,6 +62,16 @@ public class Screen
 		for ( int y = 0; y < tile.sprite.SIZE; y++ )
 		{
 			int ya = y + yp;
+			
+			for ( int x = 0; x < tile.sprite.SIZE; x++ )
+			{
+				int xa = x + xp;
+				
+				if ( xa < 0 || xa >= width || ya < 0 || ya >= height )
+					break;
+				
+				pixels[ xa + ya * width ] = tile.sprite.pixels[ x + y * tile.sprite.SIZE ];
+			}
 		}
 	}
 }
