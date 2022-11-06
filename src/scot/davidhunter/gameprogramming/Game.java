b699ec7cf8b0751fd900ead766e184e0bcf6d24d
DavidHunterScot <contact@davidhunter.scot>
2022-11-06 22:22:34 +0000

    Episode 36 - Rendering the Level!

--- src/scot/davidhunter/gameprogramming/Game.java
+++ src/scot/davidhunter/gameprogramming/Game.java
@@ -11,6 +11,8 @@ import javax.swing.JFrame;
 
 import scot.davidhunter.gameprogramming.graphics.Screen;
 import scot.davidhunter.gameprogramming.input.Keyboard;
+import scot.davidhunter.gameprogramming.level.Level;
+import scot.davidhunter.gameprogramming.level.RandomLevel;
 
 public class Game extends Canvas implements Runnable
 {
@@ -24,6 +26,7 @@ public class Game extends Canvas implements Runnable
 	private Thread thread;
 	private JFrame frame;
 	private Keyboard key;
+	private Level level;
 	private boolean running = false;
 	
 	private Screen screen;
@@ -39,6 +42,7 @@ public class Game extends Canvas implements Runnable
 		screen = new Screen( width, height );
 		frame = new JFrame();
 		key = new Keyboard();
+		level = new RandomLevel( 64, 64 );
 		
 		addKeyListener( key );
 	}
@@ -132,7 +136,7 @@ public class Game extends Canvas implements Runnable
 		}
 		
 		screen.clear();
-		screen.render( x, y );
+		level.render( x, y, screen );
 		
 		for ( int i = 0; i < pixels.length; i++ )
 		{
