e7cf0b67c64d786f0ff73c83f743ef6d4c8c4ff4
DavidHunterScot <contact@davidhunter.scot>
2022-11-06 22:00:54 +0000

    Episode 34 - Setting Offsets

--- src/scot/davidhunter/gameprogramming/graphics/Screen.java
+++ src/scot/davidhunter/gameprogramming/graphics/Screen.java
@@ -11,6 +11,8 @@ public class Screen
 	public final int MAP_SIZE = 64;
 	public final int MAP_SIZE_MASK = MAP_SIZE - 1;
 	
+	public int xOffset, yOffset;
+	
 	public int[] tiles = new int[ MAP_SIZE * MAP_SIZE ];
 	
 	private Random random = new Random();
@@ -59,6 +61,9 @@ public class Screen
 	
 	public void renderTile( int xp, int yp, Tile tile )
 	{
+		xp -= xOffset;
+		yp -= yOffset;
+		
 		for ( int y = 0; y < tile.sprite.SIZE; y++ )
 		{
 			int ya = y + yp;
@@ -73,5 +78,11 @@ public class Screen
 				pixels[ xa + ya * width ] = tile.sprite.pixels[ x + y * tile.sprite.SIZE ];
 			}
 		}
+	}
+	
+	public void setOffset( int xOffset, int yOffset )
+	{
+		this.xOffset = xOffset;
+		this.yOffset = yOffset;
 	}
 }
