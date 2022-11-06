64eed8a19386066dce1c9e51ebe94073b5fd580f
DavidHunterScot <contact@davidhunter.scot>
2022-11-06 15:52:26 +0000

    Episode 8 - The Screen Class

--- src/scot/davidhunter/gameprogramming/Game.java
+++ src/scot/davidhunter/gameprogramming/Game.java
@@ -10,6 +10,8 @@ import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
 
+import scot.davidhunter.gameprogramming.graphics.Screen;
+
 public class Game extends Canvas implements Runnable
 {
 	private static final long serialVersionUID = 1L;
@@ -22,6 +24,8 @@ public class Game extends Canvas implements Runnable
 	private JFrame frame;
 	private boolean running = false;
 	
+	private Screen screen;
+	
 	private BufferedImage image = new BufferedImage( width, height, BufferedImage.TYPE_INT_RGB );
 	private int[] pixels = ( (DataBufferInt) image.getRaster().getDataBuffer() ).getData();
 	
@@ -29,6 +33,8 @@ public class Game extends Canvas implements Runnable
 	{
 		Dimension size = new Dimension( width * scale, height * scale );
 		setPreferredSize( size );
+		
+		screen = new Screen( width, height );
 		
 		frame = new JFrame();
 	}
