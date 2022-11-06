b68b14adf8460fd7b8d57be450c82d4f6bee36f3
DavidHunterScot <contact@davidhunter.scot>
2022-11-06 20:22:09 +0000

    Episode 26 - The Tile Class

---
+++ src/scot/davidhunter/gameprogramming/level/tile/Tile.java
@@ -0,0 +1,25 @@
+package scot.davidhunter.gameprogramming.level.tile;
+
+import scot.davidhunter.gameprogramming.graphics.Screen;
+import scot.davidhunter.gameprogramming.graphics.Sprite;
+
+public class Tile
+{
+	public int x, y;
+	public Sprite sprite;
+	
+	public Tile( Sprite sprite )
+	{
+		this.sprite = sprite;
+	}
+	
+	public void render( int x, int y, Screen screen )
+	{
+		
+	}
+	
+	public boolean solid()
+	{
+		return false;
+	}
+}
