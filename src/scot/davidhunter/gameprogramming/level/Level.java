2d7ab95db601ec592e1ce2e5ad25d8483e7bf1ea
DavidHunterScot <contact@davidhunter.scot>
2022-11-07 17:12:18 +0000

    Episode 56 - Getting Tiles the Right Way

--- src/scot/davidhunter/gameprogramming/level/Level.java
+++ src/scot/davidhunter/gameprogramming/level/Level.java
@@ -5,9 +5,9 @@ import scot.davidhunter.gameprogramming.level.tile.Tile;
 
 public class Level
 {
-	protected Tile[] tiles;
 	protected int width, height;
 	protected int[] tilesInt;
+	protected int[] tiles;
 	
 	public Level( int width, int height )
 	{
@@ -56,28 +56,23 @@ public class Level
 		{
 			for ( int x = x0; x < x1; x++ )
 			{
-				// getTile( x, y ).render( x, y, screen );
-				
-				if ( x + y * 16 < 0 || x + y * 16 >= 256 )
-				{
-					Tile.voidTile.render( x, y, screen );
-					continue;
-				}
-				
-				tiles[ x + y * 16 ].render( x, y, screen );
+				getTile( x, y ).render( x, y, screen );
 			}
 		}
 	}
 	
+	// Grass = 0xFF00FF00
+	// Flower = 0xFFFFFF00
+	// Rock = 0xFF7F7F00
 	public Tile getTile( int x, int y )
 	{
 		if ( x < 0 || y < 0 || x >= width || y >= height )
 			return Tile.voidTile;
-		if ( tilesInt[ x + y * width ] == 0 )
+		if ( tiles[ x + y * width ] == 0xFF00FF00 )
 			return Tile.grass;
-		if ( tilesInt[ x + y * width ] == 1 )
+		if ( tiles[ x + y * width ] == 0xFFFFFF00 )
 			return Tile.flower;
-		if ( tilesInt[ x + y * width ] == 2 )
+		if ( tiles[ x + y * width ] == 0xFF7F7F00 )
 			return Tile.rock;
 		return Tile.voidTile;
 	}
--- src/scot/davidhunter/gameprogramming/level/SpawnLevel.java
+++ src/scot/davidhunter/gameprogramming/level/SpawnLevel.java
@@ -5,12 +5,8 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-import scot.davidhunter.gameprogramming.level.tile.Tile;
-
 public class SpawnLevel extends Level
 {
-	private int[] levelPixels;
-	
 	public SpawnLevel( String path )
 	{
 		super( path );
@@ -23,9 +19,8 @@ public class SpawnLevel extends Level
 			BufferedImage image = ImageIO.read( SpawnLevel.class.getResource( path ) );
 			int w = image.getWidth();
 			int h = image.getHeight();
-			tiles = new Tile[ w * h ];
-			levelPixels = new int[ w * h ];
-			image.getRGB( 0, 0, w, h, levelPixels, 0, w );
+			tiles = new int[ w * h ];
+			image.getRGB( 0, 0, w, h, tiles, 0, w );
 		}
 		catch ( IOException e )
 		{
@@ -34,19 +29,8 @@ public class SpawnLevel extends Level
 		}
 	}
 	
-	// Grass = 0xFF00
-	// Flower = 0xFFFF00
-	// Rock = 0x7F7F00
 	protected void generateLevel()
 	{
-		for ( int i = 0; i < levelPixels.length; i++ )
-		{
-			if ( levelPixels[ i ] == 0xff00ff00 )
-				tiles[ i ] = Tile.grass;
-			if ( levelPixels[ i ] == 0xffffff00 )
-				tiles[ i ] = Tile.flower;
-			if ( levelPixels[ i ] == 0xff7f7f00 )
-				tiles[ i ] = Tile.rock;
-		}
+		
 	}
 }
