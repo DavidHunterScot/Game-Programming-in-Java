c1b2e42e7ce90b6e7a18b06457bd6f11569cd6eb
DavidHunterScot <contact@davidhunter.scot>
2022-11-07 01:33:07 +0000

    Episode 43 - Player Input

--- src/scot/davidhunter/gameprogramming/Game.java
+++ src/scot/davidhunter/gameprogramming/Game.java
@@ -1,7 +1,9 @@
 package scot.davidhunter.gameprogramming;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
@@ -9,6 +11,7 @@ import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
 
+import scot.davidhunter.gameprogramming.entity.mob.Player;
 import scot.davidhunter.gameprogramming.graphics.Screen;
 import scot.davidhunter.gameprogramming.input.Keyboard;
 import scot.davidhunter.gameprogramming.level.Level;
@@ -27,6 +30,7 @@ public class Game extends Canvas implements Runnable
 	private JFrame frame;
 	private Keyboard key;
 	private Level level;
+	private Player player;
 	private boolean running = false;
 	
 	private Screen screen;
@@ -43,6 +47,7 @@ public class Game extends Canvas implements Runnable
 		frame = new JFrame();
 		key = new Keyboard();
 		level = new RandomLevel( 64, 64 );
+		player = new Player( key );
 		
 		addKeyListener( key );
 	}
@@ -109,20 +114,10 @@ public class Game extends Canvas implements Runnable
 		stop();
 	}
 	
-	int x = 0, y = 0;
-	
 	public void update()
 	{
 		key.update();
-		
-		if ( key.up )
-			y--;
-		if ( key.down )
-			y++;
-		if ( key.left )
-			x--;
-		if ( key.right )
-			x++;
+		player.update();
 	}
 	
 	public void render()
@@ -136,7 +131,7 @@ public class Game extends Canvas implements Runnable
 		}
 		
 		screen.clear();
-		level.render( x, y, screen );
+		level.render( player.x, player.y, screen );
 		
 		for ( int i = 0; i < pixels.length; i++ )
 		{
@@ -146,6 +141,10 @@ public class Game extends Canvas implements Runnable
 		Graphics g = bs.getDrawGraphics();
 		
 		g.drawImage( image, 0, 0, getWidth(), getHeight(), null );
+		
+		g.setColor( Color.WHITE );
+		g.setFont( new Font( "Verdana", 0, 50 ) );
+		g.drawString( "X: " + player.x + ", Y: " + player.y, 350, 300 );
 		
 		g.dispose();
 		bs.show();
