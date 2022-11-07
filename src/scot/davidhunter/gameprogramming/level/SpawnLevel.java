b1d01416b8f2c42720e875d64362484d93a98346
DavidHunterScot <contact@davidhunter.scot>
2022-11-07 16:19:44 +0000

    Episode 53 - Pixels to Tiles

--- src/scot/davidhunter/gameprogramming/level/SpawnLevel.java
+++ src/scot/davidhunter/gameprogramming/level/SpawnLevel.java
@@ -24,6 +24,7 @@ public class SpawnLevel extends Level
 			BufferedImage image = ImageIO.read( SpawnLevel.class.getResource( path ) );
 			int w = image.getWidth();
 			int h = image.getHeight();
+			tiles = new Tile[ w * h ];
 			image.getRGB( 0, 0, w, h, levelPixels, 0, w );
 		}
 		catch ( IOException e )
@@ -33,8 +34,19 @@ public class SpawnLevel extends Level
 		}
 	}
 	
+	// Grass = 0xFF00
+	// Flower = 0xFFFF00
+	// Rock = 0x7F7F00
 	protected void generateLevel()
 	{
-		
+		for ( int i = 0; i < levelPixels.length; i++ )
+		{
+			if ( levelPixels[ i ] == 0xFF00 )
+				tiles[ i ] = Tile.grass;
+			if ( levelPixels[ i ] == 0xFFFF00 )
+				tiles[ i ] = Tile.flower;
+			if ( levelPixels[ i ] == 0x7F7F00 )
+				tiles[ i ] = Tile.rock;
+		}
 	}
 }
